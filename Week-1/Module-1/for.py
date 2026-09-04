d0f0cea8c832116930bbb3f4d48fba2e3fd993c9
Rahad Ullah <rahadullah10@gmail.com>
2026-09-04 20:48:39 +0600

    while and for loop

---
+++ Week-1/Module-1/for.py
@@ -0,0 +1,13 @@
+numbers = [1, 2, 3, 4, 5]
+
+for num in numbers:
+    print(num)
+
+for num in range(5, 10):    
+    print(num)
+
+for num in range(5, 10, 2):     # range(start, end, step)
+    print(num)
+
+for char in 'hello world':
+    print(char)
