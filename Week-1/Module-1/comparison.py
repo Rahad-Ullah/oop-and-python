4ebfe0b137143e8299a4d89265d54947461e4538
Rahad Ullah <rahadullah10@gmail.com>
2026-09-04 20:06:23 +0600

    comparisions

---
+++ Week-1/Module-1/comparison.py
@@ -0,0 +1,19 @@
+# < , > , <=, >=, ==, !=
+# no: ++, --
+a = 10
+b = 3
+
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+print(a != b)
+
+power = 2 ** 3
+print(power)    # result: 8
+
+# +=, -=, *=, /=
+a = 10
+a += 5
+print(a)    # result: 15
