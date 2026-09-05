e35bb6776e494bda69c2392967ed11c22e78ff32
Rahad Ullah <rahadullah10@gmail.com>
2026-09-05 21:55:41 +0600

    lambda funciton

---
+++ Week-1/Module-3/lambda.py
@@ -0,0 +1,17 @@
+def double(x):
+    return x * 2
+
+print(double(5))
+
+# lambda function -> anonymous function
+double = lambda x: x * 2
+
+print(double(5))
+
+triple = lambda x: x * 3
+
+print(triple(5))
+
+sum = lambda a, b: a + b
+
+print(sum(5, 7))
